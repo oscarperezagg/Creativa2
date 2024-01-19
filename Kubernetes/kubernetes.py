e2eec70c625cae4ce3b2e420bf90dda1cb4b8e65
Oscar Perez Arruti <oscarperez.agg@gmail.com>
2024-01-19 18:20:24 +0100

    Error control

--- Kubernetes/kubernetes.py
+++ Kubernetes/kubernetes.py
@@ -1,57 +1,78 @@
 import subprocess
-import os 
+import os
+
+
+import subprocess
 
 
 def run_command(command):
-    """ Ejecuta un comando en la terminal y devuelve su salida. """
+    """Ejecuta un comando en la terminal y devuelve su salida y un booleano indicando el éxito."""
     try:
-        result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
-        return result.stdout, True
-    except subprocess.CalledProcessError:
-        return None, False
+        result = subprocess.run(
+            command, shell=True, check=True, text=True, capture_output=True
+        )
+        return result.stdout, None, True
+    except subprocess.CalledProcessError as e:
+        return None, e.stderr, False
+
 
 ################ CHECKS ################
 
+
 def is_docker_installed():
-    """ Verifica si Docker está instalado. """
+    """Verifica si Docker está instalado."""
     _, success = run_command("docker --version")
     return success
 
+
 def is_kubernetes_installed():
-    """ Verifica si Kubernetes está instalado. """
+    """Verifica si Kubernetes está instalado."""
     _, success = run_command("kubectl version --client")
     return success
 
+
 ################ SETUP ################
 
+
 def setup_docker():
-    """ Configura Docker en un sistema basado en Debian. """
+    """Configura Docker en un sistema basado en Debian."""
     print("\nAñadiendo la clave GPG oficial de Docker...")
-    run_command("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg")
+    run_command(
+        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg"
+    )
 
     print("\nAñadiendo el repositorio de Docker...")
-    run_command("echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null")
+    run_command(
+        "echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
+    )
 
     print("\nActualizando paquetes...")
     run_command("sudo apt update")
 
     print("\nInstalando Docker...")
-    run_command("sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y")
+    run_command(
+        "sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y"
+    )
 
     print("\nVerificando la versión de Docker...")
     version, _ = run_command("docker --version")
     print(version)
 
+
 def setup_kubernetes():
-    """ Configura Kubernetes en un sistema basado en Debian. """
+    """Configura Kubernetes en un sistema basado en Debian."""
     print("\nActualizando paquetes...")
     run_command("sudo apt update")
 
     print("\nAñadiendo la clave GPG de Kubernetes...")
-    run_command("curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg")
+    run_command(
+        "curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg"
+    )
 
     print("\nAñadiendo el repositorio de Kubernetes...")
-    run_command("echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/kubernetes.gpg] http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee -a /etc/apt/sources.list")
+    run_command(
+        "echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/kubernetes.gpg] http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee -a /etc/apt/sources.list"
+    )
 
     print("\nActualizando paquetes después de añadir el repositorio de Kubernetes...")
     run_command("sudo apt update")
@@ -65,17 +86,21 @@ def setup_kubernetes():
     print("\nVerificando la versión de kubeadm...")
     version, _ = run_command("kubeadm version")
     print(version)
-    
+
+
 def setup_images():
-    """ Configura y construye imágenes Docker para el proyecto. """
+    """Configura y construye imágenes Docker para el proyecto."""
 
     # Guardar el directorio actual
     original_directory = os.getcwd()
 
     # Clonar el repositorio de GitHub
     print("\nClonando el repositorio de GitHub...")
-    _, success = run_command("git clone https://github.com/CDPS-ETSIT/practica_creativa2.git")
+    output, error, success = run_command(
+        "git clone https://github.com/CDPS-ETSIT/practica_creativa2.git"
+    )
     if not success:
+        print("\n -> Algo fue mal! Error: \n" + (error or "Desconocido"))
         return
 
     # Cambiar al directorio específico
@@ -88,9 +113,11 @@ def setup_images():
 
     # Construir el proyecto con Docker y Gradle
     print("\nConstruyendo el proyecto con Docker y Gradle...")
-    _, success = run_command("docker run --rm -u root -v \"$(pwd)\":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build")
+    output, error, success = run_command(
+        'docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build'
+    )
     if not success:
-        print("\n -> Algo fué mal!\n")
+        print("\n -> Algo fue mal! Error: \n" + (error or "Desconocido"))
         return
 
     # Volver al directorio original
@@ -101,15 +128,16 @@ def setup_images():
         "sudo docker build -t g39/productpage -f dockerfiles/productpage .",
         "sudo docker build -t g39/details -f dockerfiles/details .",
         "sudo docker build -t g39/ratings -f dockerfiles/ratings .",
-        "sudo docker build -t g39/reviews -f practica_creativa2/bookinfo/src/reviews/Dockerfile ."
+        "sudo docker build -t g39/reviews -f practica_creativa2/bookinfo/src/reviews/Dockerfile .",
     ]
 
     for command in docker_build_commands:
         print(f"\nConstruyendo imagen Docker con el comando: {command}")
-        _, success = run_command(command)
+        output, error, success = run_command(command)
         if not success:
-            print("\n -> Algo fué mal!\n")
+            print("\n -> Algo fue mal! Error: \n" + (error or "Desconocido"))
             return
+
 
 ################ PROGRAM ################
 
@@ -127,5 +155,3 @@ else:
 
 
 setup_images()
-
-
